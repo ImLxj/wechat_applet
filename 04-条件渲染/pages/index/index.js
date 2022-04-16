f555f5ef3be16a0d978a8c8bfdfbc47f046c4bc7
李绪杰 <1340482172@qq.com>
2022-04-16 17:35:38 +0800

    条件渲染

---
+++ 04-条件渲染/pages/index/index.js
@@ -0,0 +1,22 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    type: 2
+  },
+  // 事件处理函数
+  bindViewTap() {
+
+  },
+  onLoad() {
+
+  },
+  getUserProfile(e) {
+
+  },
+  getUserInfo(e) {
+
+  }
+})
---
+++ 05-列表渲染/pages/index/index.js
@@ -0,0 +1,38 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    phone: ['小米', '华为', '苹果', 'oppo', 'vivo'],
+    person: [{
+        id: 1,
+        name: '张三',
+        age: 15
+      },
+      {
+        id: 2,
+        name: '李四',
+        age: 20
+      },
+      {
+        id: 3,
+        name: '马六',
+        age: 10
+      },
+    ]
+  },
+  // 事件处理函数
+  bindViewTap() {
+
+  },
+  onLoad() {
+
+  },
+  getUserProfile(e) {
+
+  },
+  getUserInfo(e) {
+
+  }
+})
